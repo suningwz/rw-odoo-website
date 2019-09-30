e63bd18b9bdd09a86ee6881781f8c1da76dbc035
Carlos Zuniga <carlos.zuniga@rulesware.com>
2019-09-30 15:41:15 -0600

    Updating to correct module

--- rw_security_website/__manifest__.py
+++ rw_security_website/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','website','website_blog','website_forum'],
+    'depends': ['base','website','website_blog'],
 
     # always loaded
     'data': [
